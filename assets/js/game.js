bfb4038bd939fb4e32b431e6b4bf8ba94042eefb
Jess@Turkey-Jerk <jessica1990_37@hotmail.com>
2021-06-24 20:54:16 -0600

    building js beginning

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function titled "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// fight();
